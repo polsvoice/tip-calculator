34b01f4a10614490a193c40e1952f1cee6f3ba54
Brent Bartlett <brent.bartlett1@gmail.com>
2021-08-18 16:42:38 -0700

    fixed merge

--- test/tip.spec.js
+++ test/tip.spec.js
@@ -92,7 +92,6 @@ describe("Form Validation", function(){
     expect(tipCalculator.isValid(dummyInput)).toBe(true);
   });
 
-<<<<<<< HEAD
   it("rejects undefined", function() {
     dummyInput.value = undefined;
     expect(tipCalculator.isValid(dummyInput)).toBe(false);
@@ -102,8 +101,6 @@ describe("Form Validation", function(){
     dummyInput.value = NaN;
     expect(tipCalculator.isValid(dummyInput)).toBe(false);
   });
-=======
->>>>>>> warning-output
   it("returns error message", function(){
     tipCalculator.warningMessage(true, warningOutput);
     expect(warningOutput.textContent).toEqual("Error! Please enter a numeric value");
